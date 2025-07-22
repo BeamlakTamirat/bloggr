7d1163c9480a3fb1a0bbb000bd0657c7af979f8a
BeamlakTamirat <beamlaktamirat40926102@gmail.com>
2025-07-22 23:18:18 +0300

    coment model,routes

--- backend/models/commentModel.js
+++ backend/models/commentModel.js
@@ -1,4 +1,4 @@
-const mogoose = require('mongoose');
+const mongoose = require('mongoose');
 const commentSchema = mongoose.Schema({
     user:{
         type: mongoose.Schema.Types.ObjectId,
--- backend/routes/commentRoutes.js
+++ backend/routes/commentRoutes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router({mergeParams: true});
 
-const {getCommentsForPost , createComment, getCommentsForPost } = require('../controllers/commentController.js');
+const {getCommentsForPost , createComment } = require('../controllers/commentController.js');
 const {protect} = require('../middleware/authMiddleware.js');
 
 router.route('/').get(getCommentsForPost).post(protect, createComment);
