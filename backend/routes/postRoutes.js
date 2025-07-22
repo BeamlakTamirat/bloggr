c218a431ca4cc16b59aa5cfe5d5242dace1c5327
BeamlakTamirat <beamlaktamirat40926102@gmail.com>
2025-07-22 22:29:31 +0300

    Adding comment routes to post routes

--- backend/routes/postRoutes.js
+++ backend/routes/postRoutes.js
@@ -3,7 +3,11 @@ const router = express.Router();
 const { getPosts, createPost ,getPost ,updatePost, deletePost} = require('../controllers/postController');
 const { protect } = require('../middleware/authMiddleware.js');
 
+const commentRouter = require('../routes/commentRoutes.js');
+router.use('/:postId/comments', commentRouter);
+
 router.route('/').get(getPosts).post(protect, createPost);
 router.route('/:id').get(getPost).put(protect, updatePost).delete(protect, deletePost);
+
 
 module.exports = router;
