167a6882220f170b6169cea0352501ebc6aa630c
BeamlakTamirat <beamlaktamirat40926102@gmail.com>
2025-07-22 15:29:22 +0300

    Comment model

---
+++ backend/models/commentModel.js
@@ -0,0 +1,22 @@
+const mogoose = require('mongoose');
+const commentSchema = mongoose.Schema({
+    user:{
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
+        ref: 'User',
+    },
+    post:{
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
+        ref: 'Post',
+    },
+    content: {
+        type: String,
+        required: [true,'Please add a content.'],
+    }},
+    {
+        timestamps: true,
+    }
+);
+
+module.exports = mongoose.model('Comment', commentSchema);
