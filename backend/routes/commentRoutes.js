8e3182d86eafb6b002b4c601a0b61260a75e6e27
BeamlakTamirat <beamlaktamirat40926102@gmail.com>
2025-07-22 22:28:54 +0300

    Routes for fetching and creating comments

---
+++ backend/routes/commentRoutes.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const router = express.Router({mergeParams: true});
+
+const {getCommentsForPost , createComment, getCommentsForPost } = require('../controllers/commentController.js');
+const {protect} = require('../middleware/authMiddleware.js');
+
+router.route('/').get(getCommentsForPost).post(protect, createComment);
+
+module.exports = router;
