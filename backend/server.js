fbd7f488683c1f0e80b8c9cc27f7e8cec4ebcf0e
BeamlakTamirat <beamlaktamirat40926102@gmail.com>
2025-07-31 13:09:29 +0300

    ✨ CORS

--- backend/server.js
+++ backend/server.js
@@ -3,12 +3,16 @@ const dotenv = require('dotenv');
 const connectDB = require('./config/db.js');
 const userRoutes = require('./routes/userRoutes.js');
 const postRoutes = require('./routes/postRoutes.js');
+const cors = require('cors');
 
 dotenv.config();
 
 connectDB();
 
 const app = express();
+
+app.use(cors()); 
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
